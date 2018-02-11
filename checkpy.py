ce157c8c3d60ee7195a1a02a26b7f0fac1219d2a
abewkayew@gmail.com <abewkayew@gmail.com>
2018-02-11 03:28:39 -1200

    The second version of my homeRent App

--- checkpy.py
+++ checkpy.py
@@ -1,1 +1,3 @@
 print("This is the first python trial for the sake of the git trial for the first time")
+print("This is the second print statemen")
+print("This third line is added to the git repository")
